cd365cb3d1de5bbcad76a59dfe0bf91f98af7a4e
yourboyvic <yourboyvic@gmail.com>
2016-01-24 01:12:25 -0800

    push to remote checkpoint 19_address_bloc-models
    this directory was cloned from remote then local files added then
    pushed via commit to master, am I doing this right?

---
+++ spec/entry_spec.rb
@@ -0,0 +1,34 @@
+require_relative '../models/entry'
+
+ # #1
+ RSpec.describe Entry do
+ # #2
+   describe "attributes" do
+ # #3
+     it "should respond to name" do
+       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+ # #4
+       expect(entry).to respond_to(:name)
+     end
+
+     it "should respond to phone number" do
+       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+       expect(entry).to respond_to(:phone_number)
+     end
+
+     it "should respond to email" do
+       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+       expect(entry).to respond_to(:email)
+     end
+   end
+
+   # #5
+   describe "#to_s" do
+  it "prints an entry as a string" do
+    entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+    expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
+# #6
+    expect(entry.to_s).to eq(expected_string)
+  end
+end
+end
