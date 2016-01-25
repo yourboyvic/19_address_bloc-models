b4774471fa62a90f4d1926b35075c7ef1bbe4016
yourboyvic <yourboyvic@gmail.com>
2016-01-24 18:13:39 -0800

    top bottom changes
    added lines 1-2 AND
    25-end

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,3 +1,5 @@
+require_relative 'entry'
+
 class AddressBook
    attr_accessor :entries
 
@@ -20,4 +22,17 @@ class AddressBook
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
 
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
+
+    @entries.each do |entry|
+      if name == entry.name && phone == entry.phone_number && email == entry.email
+        delete_entry = entry
+      end
+    end
+
+    @entries.delete(delete_entry)
+    end
+
+  end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,6 +18,25 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "#remove_entry" do
+    it "removes and entry from address book checking name, phone_number, and email address" do
+      book = AddressBook.new
+      book.add_entry("Jack Conti", "301.323.9009", "jack.conti@patreon.com")
+
+      name = "Ada Lovelace"
+      phone_number = "010.012.1815"
+      email_address = "augusta.king@lovelace.com"
+      book.add_entry(name, phone_number, email_address)
+
+      expect(book.entries.size).to eq 2
+      book.remove_entry(name, phone_number, email_address)
+      expect(book.entries.size).to eq 1
+      expect(book.entries.first.name).to eq("Jack Conti")
+
+    end
+  end
+
+
   describe "#add_entry" do
     it "adds only one entry to the address book" do
       book = AddressBook.new
